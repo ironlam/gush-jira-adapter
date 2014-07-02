e09626156382a1e7e2c961294bdd1f67f09f64fb
Luis Cordova <cordoval@gmail.com>
2014-07-01 19:22:13 -0500

    trim some methods not needed because we are only implementing issue tracker interface

--- src/JiraAdapter.php
+++ src/JiraAdapter.php
@@ -11,18 +11,14 @@
 
 namespace Gush\Adapter;
 
-use Github\Client;
-use Github\HttpClient\CachedHttpClient;
-use Github\ResultPager;
 use Gush\Config;
-use Gush\Exception\AdapterException;
 use Gush\Util\ArrayUtil;
-use Guzzle\Plugin\Log\LogPlugin;
+use JiraApi\Clients\IssueClient;
 
 /**
  * @author Luis Cordova <cordoval@gmail.com>
  */
-class JiraAdapter extends BaseAdapter implements IssueTracker
+class JiraAdapter implements IssueTracker
 {
     /**
      * @var string|null
@@ -62,7 +58,7 @@ class JiraAdapter extends BaseAdapter implements IssueTracker
     {
         $this->config = $config;
         $this->globalConfig = $globalConfig;
-        $this->client = $this->buildGitHubClient();
+        $this->client = $this->buildJiraClient();
     }
 
     /**
@@ -70,31 +66,21 @@ class JiraAdapter extends BaseAdapter implements IssueTracker
      */
     public function supportsRepository($remoteUrl)
     {
-        return false !== stripos($remoteUrl, 'github.com');
+        return false !== stripos($remoteUrl, 'attlasian.net');
     }
 
     /**
      * @return Client
      */
-    protected function buildGitHubClient()
+    protected function buildJiraClient()
     {
-        $cachedClient = new CachedHttpClient(
-            [
-                'cache_dir' => $this->globalConfig->get('cache-dir'),
-                'base_url' => $this->config['base_url'],
-            ]
-        );
-
-        $client = new Client($cachedClient);
-
-        if (false !== getenv('GITHUB_DEBUG')) {
-            $logPlugin = LogPlugin::getDebugPlugin();
-            $httpClient = $client->getHttpClient();
-            $httpClient->addSubscriber($logPlugin);
-        }
-
-        $client->setOption('base_url', $this->config['base_url']);
         $this->url = rtrim($this->config['base_url'], '/');
+        $client = new IssueClient(
+            $this->url,
+            $username,
+            $password
+        );
+        $client->setOption('base_url', $this->config['base_url']);
         $this->domain = rtrim($this->config['repo_domain_url'], '/');
 
         return $client;
@@ -335,299 +321,5 @@ class JiraAdapter extends BaseAdapter implements IssueTracker
             ),
             'title'
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function openPullRequest($base, $head, $subject, $body, array $parameters = [])
-    {
-        $api = $this->client->api('pull_request');
-
-        return $api->create(
-            $this->getUsername(),
-            $this->getRepository(),
-            array_merge(
-                $parameters,
-                [
-                    'base' => $base,
-                    'head' => $head,
-                    'title' => $subject,
-                    'body' => $body,
-                ]
-            )
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPullRequest($id)
-    {
-        $api = $this->client->api('pull_request');
-
-        return $this->adaptPullRequestStructure(
-            $api->show(
-                $this->getUsername(),
-                $this->getRepository(),
-                $id
-            )
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPullRequestUrl($id)
-    {
-        return sprintf('https://%s/%s/%s/pull/%d', $this->domain, $this->getUsername(), $this->getRepository(), $id);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPullRequestCommits($id)
-    {
-        $api = $this->client->api('pull_request');
-
-        $fetchedCommits = $api->commits(
-            $this->getUsername(),
-            $this->getRepository(),
-            $id
-        );
-
-        $commits = [];
-
-        foreach ($fetchedCommits as $commit) {
-            $commits[] = [
-                'sha' => $commit['sha'],
-                'user' => $commit['author']['login'],
-                'message' => $commit['commit']['message'],
-            ];
-        }
-
-        return $commits;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function mergePullRequest($id, $message)
-    {
-        $api = $this->client->api('pull_request');
-
-        $result = $api->merge(
-            $this->getUsername(),
-            $this->getRepository(),
-            $id,
-            $message
-        );
-
-        if (false === $result['merged']) {
-            throw new AdapterException('Merge failed: '.$result['message']);
-        }
-
-        return $result['sha'];
-    }
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function updatePullRequest($id, array $parameters)
-    {
-        $api = $this->client->api('pull_request');
-
-        $api->update(
-            $this->getUsername(),
-            $this->getRepository(),
-            $id,
-            $parameters
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function closePullRequest($id)
-    {
-        $this->updatePullRequest($id, ['state' => 'closed']);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPullRequests($state = null, $page = 1, $perPage = 30)
-    {
-        // FIXME is not respecting the pagination
-
-        $api = $this->client->api('pull_request');
-
-        $fetchedPrs = $api->all(
-            $this->getUsername(),
-            $this->getRepository(),
-            $state
-        );
-
-        $prs = [];
-
-        foreach ($fetchedPrs as $pr) {
-            $prs[] = $this->adaptPullRequestStructure($pr);
-        }
-
-        return $prs;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPullRequestStates()
-    {
-        return [
-            'open',
-            'closed',
-            'all',
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createRelease($name, array $parameters = [])
-    {
-        $api = $this->client->api('repo')->releases();
-
-        $release = $api->create(
-            $this->getUsername(),
-            $this->getRepository(),
-            array_merge(
-                $parameters,
-                [
-                    'tag_name' => $name
-                ]
-            )
-        );
-
-        return [
-            'url' => $release['html_url'],
-            'id' => $release['id'],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getReleases()
-    {
-        $api = $this->client->api('repo')->releases();
-
-        $fetchedReleases = $api->all(
-            $this->getUsername(),
-            $this->getRepository()
-        );
-
-        $releases = [];
-
-        foreach ($fetchedReleases as $release) {
-            $releases[] = [
-                'url' => $release['html_url'],
-                'id' => $release['id'],
-                'name' => $release['name'],
-                'tag_name' => $release['tag_name'],
-                'body' => $release['body'],
-                'draft' => $release['draft'],
-                'prerelease' => $release['prerelease'],
-                'created_at' => new \DateTime($release['created_at']),
-                'updated_at' => !empty($release['updated_at']) ? new \DateTime($release['updated_at']) : null,
-                'published_at' => !empty($release['published_at']) ? new \DateTime($release['published_at']) : null,
-                'user' => $release['user']['login'],
-            ];
-        }
-
-        return $releases;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function removeRelease($id)
-    {
-        $api = $this->client->api('repo')->releases();
-
-        $api->remove(
-            $this->getUsername(),
-            $this->getRepository(),
-            $id
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createReleaseAssets($id, $name, $contentType, $content)
-    {
-        $api = $this->client->api('repo')->releases()->assets();
-
-        $asset = $api->create(
-            $this->getUsername(),
-            $this->getRepository(),
-            $id,
-            $name,
-            $contentType,
-            $content
-        );
-
-        return $asset['id'];
-    }
-
-    protected function adaptIssueStructure(array $issue)
-    {
-        return [
-            'url' => $issue['html_url'],
-            'number' => $issue['number'],
-            'state' => $issue['state'],
-            'title' => $issue['title'],
-            'body' => $issue['body'],
-            'user' => $issue['user']['login'],
-            'labels' => ArrayUtil::getValuesFromNestedArray($issue['labels'], 'name'),
-            'assignee' => $issue['assignee']['login'],
-            'milestone' => $issue['milestone']['title'],
-            'created_at' => new \DateTime($issue['created_at']),
-            'updated_at' => !empty($issue['updated_at']) ? new \DateTime($issue['updated_at']) : null,
-            'closed_by' => !empty($issue['closed_by']) ? $issue['closed_by']['login'] : null,
-            'pull_request' => isset($issue['pull_request']),
-        ];
-    }
-
-    protected function adaptPullRequestStructure(array $pr)
-    {
-        return [
-            'url' => $pr['html_url'],
-            'number' => $pr['number'],
-            'state' => $pr['state'],
-            'title' => $pr['title'],
-            'body' => $pr['body'],
-            'labels' => [],
-            'milestone' => null,
-            'created_at' => new \DateTime($pr['created_at']),
-            'updated_at' => !empty($pr['updated_at']) ? new \DateTime($pr['updated_at']) : null,
-            'user' => $pr['user']['login'],
-            'assignee' => null,
-            'merge_commit' => null, // empty as GitHub doesn't provide this yet, merge_commit_sha is deprecated and not meant for this
-            'merged' => isset($pr['merged_by']) && isset($pr['merged_by']['login']),
-            'merged_by' => isset($pr['merged_by']) && isset($pr['merged_by']['login']) ? $pr['merged_by']['login'] : '',
-            'head' => [
-                'ref' => $pr['head']['ref'],
-                'sha' => $pr['head']['sha'],
-                'user' => $pr['head']['user']['login'],
-                'repo' => $pr['head']['repo']['name'],
-            ],
-            'base' => [
-              'ref' => $pr['base']['ref'],
-              'label' => $pr['base']['label'],
-              'sha' => $pr['base']['sha'],
-              'repo' => $pr['base']['repo']['name'],
-            ],
-        ];
     }
 }
